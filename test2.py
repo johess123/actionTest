ea1c4776fe2aee9028ff16b46ffb2ab1a1b85e62
johess123 <s109213067@mail1.ncnu.edu.tw>
2023-07-20 16:12:09 +0800

    fix_test2

--- test2.py
+++ test2.py
@@ -1,4 +1,8 @@
+import datetime
+
 def main():
-	print("git push 觸發 test2.py")
+	with open('test2.txt', 'a') as f:
+		f.write("push 時間:"+str(datetime.datetime.now())+"\n")
+		f.write("-------------------------------------\n")
 
 main()
