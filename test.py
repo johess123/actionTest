1138dab37e7eb0e83726bd8658c87440025e3dcc
johess123 <s109213067@mail1.ncnu.edu.tw>
2023-07-20 16:20:06 +0800

    fix_all_files

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import datetime
 
 def main():
-	with open('test.txt', 'a') as f:
-		f.write("現在時間:"+str(datetime.datetime.now())+"\n")
-		f.write("-------------------------------------\n")
+	print("現在時間:"+str(datetime.datetime.now()))
 
 main()
--- test2.py
+++ test2.py
@@ -1,9 +1,6 @@
 import datetime
 
 def main():
-	print("push 觸發")
-	with open('test2.txt', 'a') as f:
-		f.write("push 時間:"+str(datetime.datetime.now())+"\n")
-		f.write("-------------------------------------\n")
+	print("push 觸發時間: "+str(datetime.datetime.now())+"\n")
 
 main()
