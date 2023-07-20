010496a57f401473fccc53bb7500b9518ef1f649
johess123 <s109213067@mail1.ncnu.edu.tw>
2023-07-20 16:45:46 +0800

    add_bs4

--- test2.py
+++ test2.py
@@ -1,6 +1,22 @@
 import datetime
+import requests
+from bs4 import BeautifulSoup
 
 def main():
-	print("push 觸發時間: "+str(datetime.datetime.now())+"\n")
+	print("push 觸發時間: "+str(datetime.datetime.now()))
+	print("開始爬蟲...")
+	with open('url.txt', 'r') as f:
+		data = f.read()
+	print(data)
+	headers = {
+		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+	}
+	r = requests.get(data, headers=headers) #將網頁資料GET下來
+	soup = BeautifulSoup(r.text,"html.parser")
+	articles = soup.find_all('article', class_="b-block--top-bord")
+	for i in range(len(articles)):
+		name = articles[i].find('a', class_="js-job-link").text
+		print("----------------------------")
+		print(i+1,"工作名稱",name)
 
 main()
