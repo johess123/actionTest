3f074cede4c0cca4bb38cd0241d6e389a0eaf565
johess123 <s109213067@mail1.ncnu.edu.tw>
2023-07-20 16:15:16 +0800

    fix_test2_write

--- test2.py
+++ test2.py
@@ -1,6 +1,7 @@
 import datetime
 
 def main():
+	print("push 觸發")
 	with open('test2.txt', 'a') as f:
 		f.write("push 時間:"+str(datetime.datetime.now())+"\n")
 		f.write("-------------------------------------\n")
